03f8adde695260ef8538ef24af15e815459ae31a
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-12 16:53:59 -0600

    progress

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ module.exports = {
       backgroundImage: {
         'grain-dust': "url('/img/dust.png')",
         'grain-dust2': "url('/img/dust2.png')",
+        'grad1': "url('/img/grad1.png')",
+        'grad1v': "url('/img/grad1.svg')",
+        'grad2': "url('/img/grad2.png')",
+        'grad2v': "url('/img/grad2.svg')",
+        'grad3': "url('/img/grad3.png')",
+        'grad3v': "url('/img/grad3.svg')",
+        'cs1': "url('/img/cs1.svg')",
+        'cs2': "url('/img/cs2.svg')",
+        'cs3': "url('/img/cs3.svg')",
       }
     },
   },
