7a6eeb990e2764bc0ff36d59c2b81e7cb26158db
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-12 14:25:45 -0600

    project images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'bp600': '600px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      backgroundImage: {
+        'grain-dust': "url('/img/dust.png')",
+        'grain-dust2': "url('/img/dust2.png')",
+      }
+    },
   },
   plugins: [],
 }
