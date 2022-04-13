c41d66bb0cdee0ac72adfc0ddeda90232a8b4a49
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-13 15:49:35 -0600

    Update ProjectImage.js

--- components/ProjectImage.js
+++ components/ProjectImage.js
@@ -1,5 +1,5 @@
 const ProjectImage = (props) => (  
-  <div className='-order-5 self-start grid border-2 rounded-lg border-stone-200 p-1'>
+  <div className='-order-5 self-start grid border-2 rounded-lg border-stone-400 p-1'>
     <img 
       {...props}
       className='rounded w-full h-40 object-cover bp600:h-44 md:h-48'
