6dac0f5cd5d5b23832e7478e4b348e4e66876c89
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-20 19:45:38 -0600

    move font links to document

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,10 @@ export default function Document() {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,400&display=swap" rel="stylesheet" />
+
         <meta property="og:image" content="https://rendo.ca/og-image.png"/>
         <meta property="og:image:width" content="1200"/>
         <meta property="og:image:height" content="630"/>
