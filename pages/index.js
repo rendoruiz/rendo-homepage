8cae44f3b9a521199d6364088236e1deb7bbfca5
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-21 10:41:46 -0600

    remove robots meta tag

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,6 @@ export default function Home() {
       <Head>
         <title>Rendo Ruiz - Home</title>
         <meta name="description" content="A space cat lost in space." />
-        <meta name="robots" content="nosnippet" />
         
         <meta property="og:title" content="Rendo Ruiz - Home" />
         <meta property="og:description" content="A space cat lost in space." />
