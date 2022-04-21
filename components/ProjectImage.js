4e1df611fe76013cff72ab9162b9ff03587c6824
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-20 19:51:24 -0600

    default image alt text

--- components/ProjectImage.js
+++ components/ProjectImage.js
@@ -2,6 +2,7 @@ const ProjectImage = (props) => (
   <div className='-order-5 self-start grid border-2 rounded-lg border-stone-400 p-1'>
     <img 
       {...props}
+      alt={props.alt ?? 'Untitled image'}
       className='rounded w-full h-40 object-cover bp600:h-44 md:h-48'
     />
   </div>
