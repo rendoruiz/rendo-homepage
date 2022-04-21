3bd2a8e8d7a3d9620a546174a570f976fdd06096
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-21 11:21:26 -0600

    fix link

--- pages/404.js
+++ pages/404.js
@@ -5,11 +5,9 @@ const Custom404 = () => (
     <div className='grid px-5 py-8 font-extralight text-center'>
       <h2 className='font-light text-3xl bp360:text-4xl md:text-5xl'>Blackhole reached</h2>
       <p className='mt-2 text-xl tracking-wide md:mt-3 md:text-3xl'>There&apos;s nothing here.</p>
-      <Link
-        href='/'
-        className='justify-self-center border-2 border-stone-200 rounded-lg mt-8 px-5 py-2 font-normal text-sm text-center leading-tight tracking-wider uppercase transition-all hover:bg-stone-200 hover:text-black active:outline active:outline-2 active:outline-offset-4 active:outline-stone-200 md:mt-10 md:text-base'
-      >
-        <a>Warp back home</a>
+      <Link href='/'>
+        <a className='justify-self-center border-2 border-stone-200 rounded-lg mt-8 px-5 py-2 font-normal text-sm text-center leading-tight tracking-wider uppercase transition-all hover:bg-stone-200 hover:text-black active:outline active:outline-2 active:outline-offset-4 active:outline-stone-200 md:mt-10 md:text-base'>
+        Warp back home</a>
       </Link>
     </div>
   </div>
